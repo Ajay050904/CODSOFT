65dd4adb02301122563baf39a2a23d9a70d64934
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2024-08-05 13:33:19 +0530

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -60,4 +60,9 @@ public class ATM {
         }
         checkBalance();
     }
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(1000.00); // Initialize account with a balance of $1000
+        ATM atm = new ATM(account);
+        atm.showMenu();
+    }
 }
